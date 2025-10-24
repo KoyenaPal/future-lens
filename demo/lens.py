541cf43f3f2b5917789b3076b3dd5b65bb10e02e
Koyena Pal <pal.k@northeastern.edu>
2025-10-24 12:40:56 -0400

    updated future lens demo with the latest nnsight version

--- demo/lens.py
+++ demo/lens.py
@@ -20,21 +20,19 @@ def show_future_lens(model, tok, prefix, context, in_layer = 13, tgt_in_layer = 
     
     context = context.detach()
     num_layers = len(model.transformer.h)
-
-    with model.generate(max_new_tokens=num_toks_gen, pad_token_id=50256, remote=remote) as generator:
-        with generator.invoke(prefix) as invoker:
-            transplant_hs = model.transformer.h[in_layer].output[0].t[prefix_pos].save()
-            overall_hs = []
-            init_logits = []
+    overall_hs = []
+    init_logits = []
+    # with model.generate(max_new_tokens=num_toks_gen, pad_token_id=50256, remote=remote) as generator:
+    with model.trace(max_new_tokens=num_toks_gen, pad_token_id=50256, remote=remote) as tracer:
+        with tracer.invoke(prefix) as invoker:
             for i in range(num_layers):
                 init_logits.append(get_scores(model, model.transformer.h[i].output[0]).save())
                 overall_hs.append(model.transformer.h[i].output[0].save())
     
-    output = generator.output
-    print(tok.decode(output[0][prefix_pos+1:]))
+    output = init_logits[-1]
 
-    first_set_logits = [curr_hs.value for curr_hs in init_logits]
-    hs = [curr_hs.value for curr_hs in overall_hs]
+    first_set_logits = [curr_hs for curr_hs in init_logits]
+    hs = [curr_hs for curr_hs in overall_hs]
     first_set_logits = torch.stack(first_set_logits)
     counter = 0
     future_outputs = []
@@ -43,30 +41,25 @@ def show_future_lens(model, tok, prefix, context, in_layer = 13, tgt_in_layer = 
         curr_future_outputs = []
         curr_future_preds = []
         for x in range(0, curr_hs.shape[1]):
-            sub_hs = curr_hs[:,x,:][None,:]
-            with model.generate(max_new_tokens=num_toks_gen, pad_token_id=50256, remote=remote) as generator2:
-                with generator2.invoke("_ _ _ _ _ _ _ _ _ _") as invoker:
-                    model.transformer.wte.output = context[None,:]
-                    model.transformer.h[tgt_in_layer].output[0].t[context_pos] = sub_hs
-                    invoker.next()
-                    future_output_logits = model.lm_head.output.save()
-                    invoker.next()
-                    future_output_logits_next = model.lm_head.output.save()
-                    invoker.next()
-                    future_output_logits_next_next = model.lm_head.output.save()
-            counter+= 1
-            curr_output = torch.squeeze(future_output_logits.value,0)
-            curr_output_next = torch.squeeze(future_output_logits_next.value,0)
-            curr_output_next_next = torch.squeeze(future_output_logits_next_next.value,0)
-
-            curr_fav_tok_pred, curr_fav_tok = get_prob_tokens(curr_output[0], topk=1)
-            curr_fav_tok_pred_next, curr_fav_tok_next = get_prob_tokens(curr_output_next[0], topk=1)
-            curr_fav_tok_pred_next_next, curr_fav_tok_next_next = get_prob_tokens(curr_output_next_next[0], topk=1)
-            curr_future_outputs.append([tok.decode(curr_fav_tok),tok.decode(curr_fav_tok_next),tok.decode(curr_fav_tok_next_next)])
-            curr_future_preds.append([curr_fav_tok_pred[0].item(), curr_fav_tok_pred_next[0].item(), curr_fav_tok_pred_next_next[0].item()])
+            sub_hs = curr_hs[:,x,:].unsqueeze(0)
+            future_output_logits = list()
+            with model.generate(max_new_tokens=num_toks_gen, pad_token_id=50256, remote=remote) as generator:
+                with generator.invoke("_ _ _ _ _ _ _ _ _ _") as invoker:
+                    with generator.iter[:] as idx:
+                        if idx == 0:
+                            model.transformer.wte.output = context.unsqueeze(0)
+                            model.transformer.h[tgt_in_layer].output[0][:,context_pos,:] = sub_hs
+                        else:
+                            future_output_logits.append(model.lm_head.output.save())
+            future_output_logits = torch.cat(future_output_logits, dim=1)
+            curr_output = torch.squeeze(future_output_logits,0)
+            #shape of curr output
+            curr_fav_tok_pred, curr_fav_tok = get_prob_tokens(curr_output, topk=1)
+            curr_future_outputs.append(tok.batch_decode(curr_fav_tok))
+            curr_future_preds.append(curr_fav_tok_pred.flatten().cpu().numpy())
+            
         future_outputs.append(curr_future_outputs)
         future_preds.append(curr_future_preds)
-        
     # The full decoder head normalizes hidden state and applies softmax at the end.
     favorite_probs, favorite_tokens = get_prob_tokens(first_set_logits, topk=topk)
     
